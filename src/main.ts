82b3da220f153dda2ffa8070d664b9ed03bd2243
harshnatuskar <harshnatuskar10@gmail.com>
2024-01-25 00:19:39 +0530

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css'  
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
+  <div class="top-container">
+    <input type="url" placeholder="Enter Product URL" spellcheck="false" />
+    <a href="https://gumroad.com" target="_blank">
+      <button class="btn" id="btn1">Submit</button>
+      <button class="btn" id="btn2"></button>
+      <button class="btn" id="btn3"></button>
+    </a>
+  </div>
 `
 
